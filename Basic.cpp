954cc1ea469dec0ad36fbe2f5908d3e00d9985c8
Mahmudul-Kabir <59996575+Mahmudul-Kabir@users.noreply.github.com>
2021-05-19 10:54:42 +0600

    Create Basic.cpp

---
+++ Basic.cpp
@@ -0,0 +1,39 @@
+/*
+    
+    Note        :           
+    
+    Link        :       
+    
+*/
+
+
+#include "bits/stdc++.h"
+ 
+#define sp <<" "
+#define el <<"\n"
+#define S second
+#define F first
+#define pb push_back
+#define all(ar) ar.begin(),ar.end() 
+#define pii pair<ll,ll> 
+ 
+ 
+using namespace std;
+using ll = long long;
+using ld = long double;
+ 
+const ll mod = 1000000007;
+const ll oo = 337203600000000000;
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//*
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+//*/
+    
+    return 0;
+}
